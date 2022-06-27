7fb65f868b5af4a4da5ddb4c7b1a3005cb5d885f
dwi prihtapambudi <prihtapambudi@gmail.com>
2022-06-27 07:51:52 +0700

    add file to server

---
+++ src/main/java/com/domain/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.domain.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.domain.models.entities.Product;
+import com.domain.services.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+
+    @PostMapping
+    public Product create(@RequestBody Product product){
+        return productService.save(product);
+    }
+
+    @GetMapping
+    public Iterable<Product> findAll(){
+        return productService.findAll();
+    }
+}
